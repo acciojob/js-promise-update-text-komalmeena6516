8d0198e0684467582b3375eb362716da170370dc
Komal Meena <80938016+komalmeena6516@users.noreply.github.com>
2023-03-19 12:30:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function promise(){
+	return new Promise((resolve, reject)=>{
+		setTimeout(()=>{
+			resolve("Hello, world!");
+		}, 1000)
+	})
+}
+
+promise().then((res)=>{
+	document.getElementById("output").innerHTML = res;
+})
